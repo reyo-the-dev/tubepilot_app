fd8ada6584f5a4f3435ba7611a66fd2fdeb21ca7
Raja <raja.rathinam@pcisage.com>
2026-04-30 12:50:42 +0530

    outro

--- src/components/screens/slides/create-history-slides/create-history-slides.jsx
+++ src/components/screens/slides/create-history-slides/create-history-slides.jsx
@@ -7,7 +7,7 @@ import axios from "axios";
 import { Calendar2Month } from "react-bootstrap-icons";
 import CustomInput from "@/components/ui/custom_input/custom_input";
 import CustomTextarea from "@/components/ui/custom_textarea/custom_textarea";
-import { Image, Modal } from "react-bootstrap";
+import { Col, Image, Modal, Row } from "react-bootstrap";
 import {
   useGenerateImageForSlide,
   useGenerateSlidesForEvents,
@@ -15,10 +15,20 @@ import {
 
 const CreateHistorySlides = () => {
   const [events, setEvents] = useState(null);
+  const [historyDate, setDate] = useState(null);
 
   const fetchHistory = async () => {
     try {
-      const res = await axios.get("https://history.muffinlabs.com/date");
+      const date = new Date();
+
+      const day = date.getDate();
+      const month = date.getMonth();
+
+      const res = await axios.get(
+        `https://history.muffinlabs.com/date/${month + 1}/${day}`,
+      );
+
+      setDate(res.data.date);
 
       const eventsRes = res.data.data.Events.map((e) => ({
         ...e,
@@ -60,14 +70,18 @@ const CreateHistorySlides = () => {
 
   const [showPreview, setShowPreview] = useState(null);
 
-  const generateSlidesForEvents = async (eventTogen, index, isIntro) => {
+  const generateSlidesForEvents = async (eventTogen, index, isIntro, isOutro) => {
     try {
       const res = await generateSlidesForEventsAsync({
         event: eventTogen,
         isIntro,
+        isOutro,
       });
       setShowPreview(res.data.publicUrl);
-      if (!isIntro) {
+
+      const isOutroOrIntro = isIntro || isOutro
+
+      if (!isOutroOrIntro) {
         setSelectedEvents((prev) => {
           const out = [...prev];
           out[index].slideImageUrl = res.data.publicUrl;
@@ -99,6 +113,7 @@ const CreateHistorySlides = () => {
 
           {events && (
             <div>
+              <p>Date : {historyDate}</p>
               <p>
                 Selected: {selectedEvents.length} / {events.length}
               </p>
@@ -122,136 +137,151 @@ const CreateHistorySlides = () => {
                     Next
                   </CustomButton>
                 )}
+                {currentStep == 2 && (
+                  <CustomButton
+                    isLoading={generateSlidesForEventsIsPending}
+                    onClick={() => {
+                      generateSlidesForEvents({}, {}, false, true);
+                    }}
+                  >
+                    GetOutro
+                  </CustomButton>
+                )}
               </div>
             </div>
           )}
         </CustomBox>
       </div>
 
-      {events &&
-        currentStep === 1 &&
-        events.map((event, eventIdx) => {
-          const isSelected = selectedEvents.some((e) => e.id === event.id);
+      {events && currentStep === 1 && (
+        <Row>
+          {events.map((event, eventIdx) => {
+            const isSelected = selectedEvents.some((e) => e.id === event.id);
 
-          return (
-            <CustomBox
-              key={`event_${eventIdx}`}
-              title={event.year}
-              icon={<Calendar2Month />}
-              leftBorder={isSelected}
-            >
-              <p>{event.text}</p>
-              <br />
+            return (
+              <Col key={`event_${eventIdx}`} xs={12} lg={6}>
+                <CustomBox
+                  title={event.year}
+                  icon={<Calendar2Month />}
+                  leftBorder={isSelected}
+                >
+                  <p>{event.text}</p>
+                  <br />
 
-              <CustomButton
-                onClick={() => {
-                  if (isSelected) {
-                    setSelectedEvents((prev) =>
-                      prev.filter((pse) => pse.id !== event.id),
-                    );
-                  } else {
-                    setSelectedEvents((prev) => [...prev, event]);
-                  }
-                }}
-              >
-                {isSelected ? "Remove" : "Select"}
-              </CustomButton>
-            </CustomBox>
-          );
-        })}
+                  <CustomButton
+                    onClick={() => {
+                      if (isSelected) {
+                        setSelectedEvents((prev) =>
+                          prev.filter((pse) => pse.id !== event.id),
+                        );
+                      } else {
+                        setSelectedEvents((prev) => [...prev, event]);
+                      }
+                    }}
+                  >
+                    {isSelected ? "Remove" : "Select"}
+                  </CustomButton>
+                </CustomBox>
+              </Col>
+            );
+          })}
+        </Row>
+      )}
 
       {selectedEvents && currentStep === 2 && (
-        <div>
+        <Row>
           {selectedEvents.map((selectedEvent, selectedEventIdx) => {
             const isSelected = selectedEvents.some(
               (e) => e.id === selectedEvent.id,
             );
 
             return (
-              <CustomBox
-                key={`selectedEvent${selectedEvent.id}`}
-                title={selectedEvent.year}
-                icon={<Calendar2Month />}
-                leftBorder={isSelected}
-              >
-                <CustomTextarea value={selectedEvent.text} label={"Text"} />
-                <br />
-                <CustomInput
-                  value={selectedEventIdx.imagePropmt}
-                  label={"Highlights"}
-                  onChange={(e, v) => {
-                    setSelectedEvents((prev) => {
-                      const out = [...prev];
-                      out[selectedEventIdx].highlights = v;
-                      return out;
-                    });
-                  }}
-                />
-                <br />
-                <CustomTextarea
-                  value={selectedEvent.imagePrompt}
-                  label={"Image Prompt"}
-                  onChange={(e, v) => {
-                    setSelectedEvents((prev) => {
-                      const out = [...prev];
-                      out[selectedEventIdx].imagePrompt = v;
-                      return out;
-                    });
-                  }}
-                />
-                <br />
-                <div className={styles.images}>
-                  {selectedEvent.imageUrl && (
-                    <Image src={selectedEvent.imageUrl} fluid alt="xx" />
-                  )}
-                  {selectedEvent.slideImageUrl && (
-                    <Image src={selectedEvent.slideImageUrl} fluid alt="xx" />
-                  )}
-                </div>
-                <br />
-                <br />
-                <div className={styles.btns}>
-                  <CustomButton
-                    onClick={async () => {
-                      await generateImageForEvent(
-                        selectedEvent,
-                        selectedEventIdx,
-                      );
+              <Col key={`selectedEvent${selectedEvent.id}`} xs={12} lg={6}>
+                <CustomBox
+                  title={selectedEvent.year}
+                  icon={<Calendar2Month />}
+                  leftBorder={isSelected}
+                >
+                  <CustomTextarea value={selectedEvent.text} label={"Text"} />
+                  <br />
+                  <CustomInput
+                    value={selectedEventIdx.imagePropmt}
+                    label={"Highlights"}
+                    onChange={(e, v) => {
+                      setSelectedEvents((prev) => {
+                        const out = [...prev];
+                        out[selectedEventIdx].highlights = v;
+                        return out;
+                      });
                     }}
-                    isLoading={isPending}
-                  >
-                    Generate Image
-                  </CustomButton>
+                  />
+                  <br />
+                  <CustomTextarea
+                    value={selectedEvent.imagePrompt}
+                    label={"Image Prompt"}
+                    onChange={(e, v) => {
+                      setSelectedEvents((prev) => {
+                        const out = [...prev];
+                        out[selectedEventIdx].imagePrompt = v;
+                        return out;
+                      });
+                    }}
+                  />
+                  <br />
+                  <div className={styles.images}>
+                    {selectedEvent.imageUrl && (
+                      <Image src={selectedEvent.imageUrl} fluid alt="xx" />
+                    )}
+                    {selectedEvent.slideImageUrl && (
+                      <Image src={selectedEvent.slideImageUrl} fluid alt="xx" />
+                    )}
+                  </div>
+                  <br />
+                  <br />
+                  <div className={styles.btns}>
+                    <CustomButton
+                      onClick={async () => {
+                        await generateImageForEvent(
+                          selectedEvent,
+                          selectedEventIdx,
+                        );
+                      }}
+                      isLoading={isPending}
+                    >
+                      Generate Image
+                    </CustomButton>
 
-                  <CustomButton
-                    onClick={async () => {
-                      await generateSlidesForEvents(
-                        selectedEvent,
-                        selectedEventIdx,
-                      );
-                    }}
-                    isLoading={generateSlidesForEventsIsPending}
-                  >
-                    Generate Slide
-                  </CustomButton>
-                  {selectedEvent.imageUrl && (
                     <CustomButton
                       onClick={async () => {
                         await generateSlidesForEvents(
                           selectedEvent,
-                          selectedEventIdx,true
+                          selectedEventIdx,
                         );
                       }}
                       isLoading={generateSlidesForEventsIsPending}
                     >
-                      Generate Intro
+                      Generate Slide
                     </CustomButton>
-                  )}
-                </div>
-              </CustomBox>
+                    {selectedEvent.imageUrl && (
+                      <CustomButton
+                        onClick={async () => {
+                          await generateSlidesForEvents(
+                            selectedEvent,
+                            selectedEventIdx,
+                            true,
+                          );
+                        }}
+                        isLoading={generateSlidesForEventsIsPending}
+                      >
+                        Generate Intro
+                      </CustomButton>
+                    )}
+                  </div>
+                </CustomBox>
+              </Col>
             );
           })}
-        </div>
+        </Row>
       )}
     </>
   );
